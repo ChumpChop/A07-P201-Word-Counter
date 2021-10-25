495b13cd39258f685d64a945d3d25a54fd9986f8
Clive B Harrison <clive_b_harrison@outlook.com>
2021-10-25 22:36:31 +0100

    Add project files.

---
+++ WPF Word Counter/MainWindow.xaml.vb
@@ -0,0 +1,72 @@
+﻿Class MainWindow 
+
+    ''' <summary>
+    ''' Count the characters in a block of text
+    ''' </summary>
+    ''' <param name="text">The string containing the text to count
+    ''' characters in</param>
+    ''' <returns>The number of characters in the string</returns>
+    ''' <remarks></remarks>    
+    Private Function CountCharacters(ByVal text As String) As Integer
+        Return text.Length
+    End Function
+
+    Private Sub txtWords_TextChanged(ByVal sender As Object, _
+        ByVal e As System.Windows.Controls.TextChangedEventArgs) _
+        Handles txtWords.TextChanged
+
+        'Something changed to display the results
+        UpdateDisplay()
+
+    End Sub
+
+    ''' <summary>
+    ''' Count the number of words in a block of text
+    ''' </summary>
+    ''' <param name="text">The string containing the text to count</param>
+    ''' <returns>The number of words in the string</returns>
+    ''' <remarks></remarks>
+    Private Function CountWords(ByVal text As String) As Integer
+        'Is the text empty?
+        If text.Trim.Length = 0 Then Return 0
+
+        'Split the words
+        Dim strWords() As String = text.Split(" "c)
+
+        'Return the number of words
+        Return strWords.Length
+    End Function
+
+    Private Sub UpdateDisplay()
+        'If the window has not completed initialization then exit
+        'this procedure as the radCountWords radio button has not
+        'been created yet
+        If Not Me.IsInitialized Then Exit Sub
+
+        'Do we want to count words?
+        If radCountWords.IsChecked Then
+            'Update the results with words
+            lblResults.Content = CountWords(txtWords.Text) & " words"
+        Else
+            'Update the results with characters
+            lblResults.Content = CountCharacters(txtWords.Text) & " characters"
+        End If
+    End Sub
+
+    Private Sub radCountWords_Checked(ByVal sender As Object, _
+        ByVal e As System.Windows.RoutedEventArgs) _
+        Handles radCountWords.Checked
+
+        'Update the display
+        UpdateDisplay()
+    End Sub
+
+    Private Sub radCountChars_Checked(ByVal sender As Object, _
+        ByVal e As System.Windows.RoutedEventArgs) _
+        Handles radCountChars.Checked
+
+        'Update the display
+        UpdateDisplay()
+    End Sub
+
+End Class
---
+++ Windows Forms Word Counter CBH/Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub txtWords_TextChanged(sender As Object, e As EventArgs) Handles txtWords.TextChanged
+        '''''Count the number of characters
+        ''''Dim intChars As Integer = CountCharacters(txtWords.Text)
+        '''''Display the results
+        ''''lblResults.Text = intChars & " characters"
+
+        'Something changed so display the results
+        UpdateDisplay()
+    End Sub
+
+    Private Sub UpdateDisplay()
+        'Do we want to count words?
+        If radCountWords.Checked Then
+            'Update the results with words
+            lblResults.Text = CountWords(txtWords.Text) & " words"
+        Else
+            'Update the results with characters
+            lblResults.Text = CountCharacters(txtWords.Text) & " characters"
+        End If
+    End Sub
+
+    Private Sub radCountWords_CheckedChanged(sender As Object, e As EventArgs) Handles radCountWords.CheckedChanged
+        'Something changed so display the results
+        UpdateDisplay()
+    End Sub
+
+    Private Sub radCountChars_CheckedChanged(sender As Object, e As EventArgs) Handles radCountChars.CheckedChanged
+        'Something changed so display the results
+        UpdateDisplay()
+    End Sub
+
+    ‘‘‘ <summary>
+    ‘‘‘ Count the characters in a block of text
+    ‘‘‘ </summary>
+    ‘‘‘ <param name="text">The string containing the text to count
+    ‘‘‘ characters in</param>
+    ‘‘‘ <returns>The number of characters in the string</returns>
+    ‘‘‘ <remarks></remarks>
+    Private Function CountCharacters(ByVal text As String) As Integer
+        Return text.Length
+    End Function
+
+    ‘‘‘ <summary>
+    ‘‘‘ Count the number of words in a block of text
+    ‘‘‘ </summary>
+    ‘‘‘ <param name="text">The string containing the text to count</param>
+    ‘‘‘ <returns>The number of words in the string</returns>
+    ‘‘‘ <remarks></remarks>
+    Private Function CountWords(ByVal text As String) As Integer
+        'Is the text empty?
+        If text.Trim.Length = 0 Then Return 0
+        'Split the words
+        Dim strWords() As String = text.Split(" "c)
+        'Return the number of words
+        Return strWords.Length
+    End Function
+End Class
